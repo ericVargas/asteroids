2e80ac6ece641fa26eafefabc316bd9e1d779ed8
ericVargas <batman@Erics-MacBook-Pro.local>
2017-04-13 19:04:18 -0400

    linked the start button to swicth the screen to the game

--- js/script.js
+++ js/script.js
@@ -8,5 +8,19 @@ function howToPlay() {
     let instruct = document.createElement('article');
     buttonUl.replaceChild(instruct, buttonUl.childNodes[1]);
     instruct.innerHTML = 'Click to destroy the astroids on the screen before the clock runs out!</br></br> If any are left after the timer ends, the game is over!';
-    document.getElementById('screen').style.margin= '40px 0px 20px 0px';
+    document.getElementById('title').style.margin= '40px 0px 20px 0px';
+};
+
+let startB = document.getElementById('buttonStart');
+let title = document.getElementById('titleP');
+
+startB.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('Start the game!');
+    document.getElementById('screen').id = 'screen2';
+    title.setAttribute('class', 'off');
+    let bubble = document.createElement('div');
+    bubble.setAttribute('class', 'bubble');
+    screen2.appendChild(bubble);
 };
